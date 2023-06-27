05b726e8d7a857cd58e671b77b299d93ce48e4eb
patienceosarenagharu <belovedpatience@gmail.com>
2023-06-26 22:55:25 -0200

    first commit

---
+++ JS function.js
@@ -0,0 +1,25 @@
+
+
+
+
+//array that are number
+const my Arr =(2
+
+ 
+ function onlyNumber(Arr) {
+    let newArr=   
+   for (let 1 = 0; 1 < Arr.length; 1++) {
+   if (condition) {
+   newArr.unshift(Arr[i])
+   }
+} 
+    
+   return newArr
+console.log(onlyNub(Arr){
+
+    function filterArr(Arr) {
+
+
+ } 
+    
+    
---
+++ javascript.js
@@ -0,0 +1,5 @@
+console.log ("hello onramp")
+function showIdentity(name, age, phone) {
+    const id = 'my name is $ {name}, i am $ {age} year'
+}
+
